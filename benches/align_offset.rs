b1b5ea2d5661eded1d579f1b5a01f21c44cb2ffd
Amos Onn <amosonn@gmail.com>
2020-01-30 21:27:04 +0100

    Reduced bench to interesting part

--- benches/align_offset.rs
+++ benches/align_offset.rs
@@ -4,8 +4,8 @@ use bench_align_offset::{align_offset_old, align_offset_new};
 
 fn bench_align_offset(c: &mut Criterion) {
     let mut group = c.benchmark_group("args");
-    for (p, stride) in [(1usize, 3usize), (8, 8), (2, 10)].iter() {
-        for align in [1usize, 2, 4, 8, 16, 32, 64, 128, 256, 512].iter() {
+    for (p, stride) in [(8usize, 24usize)].iter() {
+        for align in [16usize, 32, 64, 128, 256, 512, 1024, 2048, 4096, 1<<17, 1<<20].iter() {
             let tup = (*p, *stride, *align);
             group.bench_with_input(BenchmarkId::new("old", format!("{:?}", tup)), &tup,
                 |b, i| b.iter(|| unsafe { align_offset_old(i.0, i.1, i.2) }));
@@ -20,7 +20,7 @@ criterion_group!{
     name = benches;
     config = Criterion::default()
         .warm_up_time(core::time::Duration::new(1, 0))
-        .sample_size(200);
+        .sample_size(400);
     targets = bench_align_offset
 }
 criterion_main!(benches);
